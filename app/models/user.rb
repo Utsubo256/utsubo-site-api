e734f4595450e50f1875a4886e8f9c28dd52f164
Utsubo256 <greeeentea256@gmail.com>
2023-04-17 14:35:16 +0900

    各apiが実行される前にauthenticateするよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def self.find_or_create_user(user_info)
+    uid = user_info[:uid]
+    user = User.find_by(uid: uid)
+    return user if user
+
+    User.create!(uid: uid, name: "User_#{uid[0, 4]}")
+  end
 end
