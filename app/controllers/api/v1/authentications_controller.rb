9bf3977e1fbd3dc832d326557f8994ed12e22145
Utsubo256 <greeeentea256@gmail.com>
2023-04-18 14:24:20 +0900

    AuthenticationsControllerを更新

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::AuthenticationsController < ApplicationController
+  def create
+    return unless current_user
+
+    render json: current_user, serializer: UserSerializer, status: :created
+  end
 end
