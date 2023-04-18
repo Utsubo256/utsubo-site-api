46283b12ba3ce9aac344e9f926cd42ce0dd41d0e
Utsubo256 <greeeentea256@gmail.com>
2023-04-18 12:09:04 +0900

    rubocopの警告について対応

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,2 +1,2 @@
-class Api::V1::AuthenticationsController < ApplicationController  
+class Api::V1::AuthenticationsController < ApplicationController
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :user do
-    
+    # users
   end
 end
