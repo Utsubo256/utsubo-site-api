1a93dcd230e84900be3eb1ed13a4414ebef48416
Utsubo256 <greeeentea256@gmail.com>
2023-04-22 15:50:40 +0900

    MoraysControllerのindexアクションへのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
       post '/auth', to: 'authentications#create'
+      resources :morays, only: %i[index]
     end
   end
 end
