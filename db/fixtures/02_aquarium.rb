e08692cb398e8a91527d15ccf5d818720b1b8c09
Utsubo256 <greeeentea256@gmail.com>
2023-04-25 21:07:13 +0900

    水族館データの設定ファイルを追加

---
+++ db/fixtures/02_aquarium.rb
@@ -0,0 +1,329 @@
+Aquarium.seed(:id,
+  {
+    id: 1,
+    image: '.png',
+    name: 'おたる水族館',
+    address_city: '北海道小樽市',
+    address_detail: '北海道小樽市祝津3丁目303番地',
+    region: '北海道',
+    description: '',
+    site_url: 'https://otaru-aq.jp/',
+    business_days_hours_url: 'https://otaru-aq.jp/guide/openningtime',
+    entrance_fee_url: 'https://otaru-aq.jp/guide/tickets'
+  },
+  {
+    id: 2,
+    image: '.png',
+    name: 'アクアマリンふくしま',
+    address_city: '福島県いわき市',
+    address_detail: '福島県いわき市小名浜字辰巳町50',
+    region: '東北',
+    description: '',
+    site_url: 'https://www.aquamarine.or.jp/',
+    business_days_hours_url: '下記リンクの「開館案内」欄
+    https://www.aquamarine.or.jp/guide/info/',
+    entrance_fee_url: '下記リンクの「入館料」蘭
+    https://www.aquamarine.or.jp/guide/info/'
+  },
+  {
+    id: 3,
+    image: '.png',
+    name: 'アクアワールド茨城県大洗水族館',
+    address_city: '茨城県東茨城郡',
+    address_detail: '茨城県東茨城郡大洗町磯浜町8252-3',
+    region: '関東',
+    description: '',
+    site_url: 'https://www.aquaworld-oarai.com/',
+    business_days_hours_url: 'https://www.aquaworld-oarai.com/guide/',
+    entrance_fee_url: 'https://www.aquaworld-oarai.com/guide/admission/'
+  },
+  {
+    id: 4,
+    image: '.png',
+    name: 'しながわ水族館',
+    address_city: '東京都品川区',
+    address_detail: '東京都品川区勝島3-2-1',
+    region: '関東',
+    description: '',
+    site_url: 'https://www.aquarium.gr.jp/',
+    business_days_hours_url: 'HP内のカレンダーに記載
+    https://www.aquarium.gr.jp/',
+    entrance_fee_url: 'https://www.aquarium.gr.jp/#price'
+  },
+  {
+    id: 5,
+    image: '.png',
+    name: '葛西臨海水族園',
+    address_city: '東京都江戸川区',
+    address_detail: '東京都江戸川区臨海町6-2-3',
+    region: '関東',
+    description: '',
+    site_url: 'https://www.tokyo-zoo.net/zoo/kasai/',
+    business_days_hours_url: '「開園時間」「休園日」欄に記載
+    https://www.tokyo-zoo.net/zoo/kasai/hours.html',
+    entrance_fee_url: '「入場料」欄に記載
+    https://www.tokyo-zoo.net/zoo/kasai/hours.html'
+  },
+  {
+    id: 6,
+    image: '.png',
+    name: '横浜開運水族館',
+    address_city: '神奈川県横浜市中区',
+    address_detail: '神奈川県横浜市中区山下町144番地チャイナスクエア3F（中華街大通り沿い）',
+    region: '関東',
+    description: '',
+    site_url: 'https://fortune-aquarium.com/',
+    business_days_hours_url: 'https://fortune-aquarium.com/acess',
+    entrance_fee_url: 'https://fortune-aquarium.com/tikcet'
+  },
+  {
+    id: 7,
+    image: '.png',
+    name: '新潟市水族館マリンピア日本海',
+    address_city: '新潟県新潟市中央区',
+    address_detail: '新潟県新潟市中央区西船見町5932-445',
+    region: '中部',
+    description: '',
+    site_url: 'https://www.marinepia.or.jp/',
+    business_days_hours_url: '「開館時間・休館日」に記載
+    https://www.marinepia.or.jp/info',
+    entrance_fee_url: '「入館料・手続」下の「個人のお客様」「団体のお客様」に記載
+    https://www.marinepia.or.jp/info'
+  },
+  {
+    id: 8,
+    image: '.png',
+    name: '魚津水族館',
+    address_city: '富山県魚津市',
+    address_detail: '富山県魚津市三ケ1390',
+    region: '中部',
+    description: '',
+    site_url: 'https://www.uozu-aquarium.jp/',
+    business_days_hours_url: '「営業時間・休館日」欄に記載
+    https://www.uozu-aquarium.jp/info/',
+    entrance_fee_url: '「入館料」欄に記載
+    https://www.uozu-aquarium.jp/info/'
+  },
+  {
+    id: 9,
+    image: '.png',
+    name: '越前松島水族館',
+    address_city: '福井県坂井市',
+    address_detail: '福井県坂井市三国町崎74-2-3',
+    region: '中部',
+    description: '',
+    site_url: 'https://www.echizen-aquarium.com/',
+    business_days_hours_url: 'https://www.echizen-aquarium.com/information/hours.html',
+    entrance_fee_url: 'https://www.echizen-aquarium.com/information/price.html'
+  },
+  {
+    id: 10,
+    image: '.png',
+    name: '下田海中水族館',
+    address_city: '静岡県下田市',
+    address_detail: '静岡県下田市3-22-31',
+    region: '中部',
+    description: '',
+    site_url: 'https://shimoda-aquarium.com/',
+    business_days_hours_url: '「営業時間のご案内」欄に記載
+    https://shimoda-aquarium.com/information/fee-business-hours/',
+    entrance_fee_url: '「入場料金」欄に記載
+    https://shimoda-aquarium.com/information/fee-business-hours/'
+  },
+  {
+    id: 11,
+    image: '.png',
+    name: '竹島水族館',
+    address_city: '愛知県蒲郡市',
+    address_detail: '愛知県蒲郡市竹島町1-6',
+    region: '中部',
+    description: '',
+    site_url: 'https://www.city.gamagori.lg.jp/site/takesui/',
+    business_days_hours_url: 'https://www.city.gamagori.lg.jp/site/takesui/kyukan-kaikan.html',
+    entrance_fee_url: 'https://www.city.gamagori.lg.jp/site/takesui/onekoin.html'
+  },
+  {
+    id: 12,
+    image: '.png',
+    name: '南知多ビーチランド',
+    address_city: '愛知県知多郡',
+    address_detail: '愛知県知多郡美浜町奥田428-1',
+    region: '中部',
+    description: '',
+    site_url: 'https://beachland.jp/',
+    business_days_hours_url: 'https://beachland.jp/schedule/',
+    entrance_fee_url: 'https://beachland.jp/schedule/#ticketInfo'
+  },
+  {
+    id: 13,
+    image: '.png',
+    name: '海遊館',
+    address_city: '大阪府大阪市港区',
+    address_detail: '大阪府大阪市港区海岸通1-1-10',
+    region: '近畿',
+    description: '',
+    site_url: 'https://www.kaiyukan.com/',
+    business_days_hours_url: 'https://www.kaiyukan.com/info/hours/',
+    entrance_fee_url: 'https://www.kaiyukan.com/info/admission/'
+  },
+  {
+    id: 14,
+    image: '.png',
+    name: '和歌山県立自然博物館',
+    address_city: '和歌山県海南市',
+    address_detail: '和歌山県海南市船尾370-1',
+    region: '関西',
+    description: '',
+    site_url: 'https://www.shizenhaku.wakayama-c.ed.jp/index.html',
+    business_days_hours_url: '「開館時間」「休館日」欄に記載
+    https://www.shizenhaku.wakayama-c.ed.jp/infomation/infomation.html',
+    entrance_fee_url: '「入館料金」欄に記載
+    https://www.shizenhaku.wakayama-c.ed.jp/infomation/infomation.html'
+  },
+  {
+    id: 15,
+    image: '.png',
+    name: '京都大学白浜水族館',
+    address_city: '和歌山県西牟婁郡',
+    address_detail: '和歌山県西牟婁郡白浜町459',
+    region: '関西',
+    description: '',
+    site_url: 'https://www.seto.kyoto-u.ac.jp/aquarium/',
+    business_days_hours_url: '「「開館時間・入館料」欄に記載
+    https://www.seto.kyoto-u.ac.jp/aquarium/shisetsu-joho/eigyoujikan-nyukannryo.html',
+    entrance_fee_url: '「開館時間・入館料」欄に記載
+    https://www.seto.kyoto-u.ac.jp/aquarium/shisetsu-joho/eigyoujikan-nyukannryo.html'
+  },
+  {
+    id: 16,
+    image: '.png',
+    name: 'すさみ町立エビとカニの水族館',
+    address_city: '和歌山県西牟婁郡',
+    address_detail: '和歌山県西牟婁郡すさみ町江住808-1',
+    region: '関西',
+    description: '',
+    site_url: 'https://ebikani-aquarium.com/',
+    business_days_hours_url: '「料金」欄に記載
+    https://ebikani-aquarium.com/information/',
+    entrance_fee_url: '「開館時間」「休館日」欄に記載
+    https://ebikani-aquarium.com/information/'
+  },
+  {
+    id: 17,
+    image: '.png',
+    name: '城崎マリンワールド',
+    address_city: '兵庫県豊岡市',
+    address_detail: '兵庫県豊岡市瀬戸1090番地',
+    region: '関西',
+    description: '',
+    site_url: 'https://marineworld.hiyoriyama.co.jp/',
+    business_days_hours_url: '「入園料金」欄に記載
+    https://marineworld.hiyoriyama.co.jp/information/info01',
+    entrance_fee_url: '「営業時間」欄に記載
+    https://marineworld.hiyoriyama.co.jp/information/info01'
+  },
+  {
+    id: 18,
+    image: '.png',
+    name: '宮島水族館みやじマリン',
+    address_city: '広島県廿日市市',
+    address_detail: '広島県廿日市市宮島町10-3',
+    region: '中国',
+    description: '',
+    site_url: 'https://www.miyajima-aqua.jp/',
+    business_days_hours_url: '「入館料」欄に記載
+    https://miyajima-aqua.jp/guide/',
+    entrance_fee_url: '「営業時間」欄に記載
+    https://miyajima-aqua.jp/guide/'
+  },
+  {
+    id: 19,
+    image: '.png',
+    name: 'しまね海洋館アクアス',
+    address_city: '島根県浜田市',
+    address_detail: '島根県浜田市久代町1117番地2',
+    region: '中国',
+    description: '',
+    site_url: 'https://aquas.or.jp/',
+    business_days_hours_url: 'https://aquas.or.jp/guide/opening-closed/',
+    entrance_fee_url: 'https://aquas.or.jp/guide/admission-fee/'
+  },
+  {
+    id: 20,
+    image: '.png',
+    name: '市立しものせき水族館海響館',
+    address_city: '山口県下関市',
+    address_detail: '山口県下関市あるかぽーと6-1',
+    region: '中国',
+    description: '',
+    site_url: 'https://www.kaikyokan.com/',
+    business_days_hours_url: 'https://www.kaikyokan.com/access.php',
+    entrance_fee_url: 'https://www.kaikyokan.com/price.php'
+  },
+  {
+    id: 21,
+    image: '.png',
+    name: 'むろと廃校水族館',
+    address_city: '高知県室戸市',
+    address_detail: '高知県室戸市室戸岬町533-2',
+    region: '四国',
+    description: '',
+    site_url: 'https://www.city.muroto.kochi.jp/pages/page0343.php',
+    business_days_hours_url: '「入館料」欄に記載
+    https://www.city.muroto.kochi.jp/pages/page0343.php',
+    entrance_fee_url: '「登校・下校時間」欄に記載
+    https://www.city.muroto.kochi.jp/pages/page0343.php'
+  },
+  {
+    id: 22,
+    image: '.png',
+    name: '九十九島水族館海きらら',
+    address_city: '長崎県佐世保市',
+    address_detail: '長崎県佐世保市鹿子前町1008',
+    region: '九州',
+    description: '',
+    site_url: 'https://www.umikirara.jp/',
+    business_days_hours_url: '「料金」欄に記載
+    https://www.umikirara.jp/information/',
+    entrance_fee_url: '「営業時間」欄に記載
+    https://www.umikirara.jp/information/'
+  },
+  {
+    id: 23,
+    image: '.png',
+    name: 'いおワールドかごしま水族館',
+    address_city: '鹿児島県鹿児島市',
+    address_detail: '鹿児島県鹿児島市本港新町3-1',
+    region: '九州',
+    description: '',
+    site_url: 'https://ioworld.jp/',
+    business_days_hours_url: '「入館料」欄に記載
+    https://ioworld.jp/information',
+    entrance_fee_url: '「営業日・営業時間」欄に記載
+    https://ioworld.jp/information'
+  },
+  {
+    id: 24,
+    image: '.png',
+    name: '沖縄美ら海水族館',
+    address_city: '沖縄県国頭郡',
+    address_detail: '沖縄県国頭郡本部町石川424番地',
+    region: '沖縄',
+    description: '',
+    site_url: 'https://churaumi.okinawa/',
+    business_days_hours_url: 'https://churaumi.okinawa/guide/hour/',
+    entrance_fee_url: 'https://churaumi.okinawa/guide/info/'
+  },
+  {
+    id: 25,
+    image: '.png',
+    name: 'DMMかりゆし水族館',
+    address_city: '沖縄県豊見城市',
+    address_detail: '沖縄県豊見城市豊崎3-35',
+    region: '沖縄',
+    description: '',
+    site_url: 'https://kariyushi-aquarium.com/',
+    business_days_hours_url: 'https://kariyushi-aquarium.com/opening-hours/',
+    entrance_fee_url: 'https://kariyushi-aquarium.com/info/'
+  }
+)
