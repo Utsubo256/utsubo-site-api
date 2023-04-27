5b41b16d14c8893271a2f43ec67d7c8b9c269a57
Utsubo256 <greeeentea256@gmail.com>
2023-04-27 12:50:49 +0900

    AquariaControllerにindexアクションを追加

--- app/controllers/api/v1/aquaria_controller.rb
+++ app/controllers/api/v1/aquaria_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::AquariaController < ApplicationController
+  skip_before_action :authenticate
+
+  def index
+    aquaria = Aquarium.all
+    render json: aquaria, status: :ok
+  end
 end
