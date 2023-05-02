0e788462f1c8ff6d1b657d88cc74275329045b3c
Utsubo256 <greeeentea256@gmail.com>
2023-05-02 16:34:01 +0900

    MorayDetailSerializerに関連する水族館情報を追加

--- app/serializers/moray_detail_serializer.rb
+++ app/serializers/moray_detail_serializer.rb
@@ -1,3 +1,5 @@
 class MorayDetailSerializer < ActiveModel::Serializer
   attributes :id, :avatar, :name_ja, :name_en, :name_academic, :max_length_str, :distribution, :description, :video_url
+
+  has_many :aquaria, through: :exhibitions
 end
