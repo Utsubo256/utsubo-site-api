fb9311df8464e26953d48b3e97d9b51f747ef0e7
Utsubo256 <greeeentea256@gmail.com>
2023-05-02 15:11:52 +0900

    CreateExhibitionマイグレーションファイルを修正

--- db/migrate/20230502054746_create_exhibitions.rb
+++ db/migrate/20230502054746_create_exhibitions.rb
@@ -1,10 +1,14 @@
 class CreateExhibitions < ActiveRecord::Migration[7.0]
   def change
     create_table :exhibitions do |t|
-      t.integer moray_id
-      t.integer aquarium_id
+      t.integer :moray_id
+      t.integer :aquarium_id
+      t.datetime :discarded_at
 
       t.timestamps
     end
+    add_index :exhibitions, :moray_id
+    add_index :exhibitions, :aquarium_id
+    add_index :exhibitions, [:moray_id, :aquarium_id], unique: true
   end
 end
