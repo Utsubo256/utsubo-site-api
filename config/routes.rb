901c5744e4e5f1de3726b846f9a386a5271b8fe3
Utsubo256 <greeeentea256@gmail.com>
2023-05-09 12:21:54 +0900

    ウツボ詳細、水族館詳細にコメント一覧取得・投稿・削除のためのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   namespace :api, format: 'json' do
     namespace :v1 do
       post '/auth', to: 'authentications#create'
-      resources :morays, only: %i[index show]
-      resources :aquaria, only: %i[index show]
+      resources :morays, only: %i[index show] do
+        resources :moray_comments, only: %i[index create destroy]
+      end
+      resources :aquaria, only: %i[index show] do
+        resources :aquarium_comments, only: %i[index create destroy]
+      end
       resource :profile, only: %i[show update]
     end
   end
