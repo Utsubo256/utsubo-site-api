fafd2c77abf97afd7eff8de174f2fd6ced743453
Utsubo256 <greeeentea256@gmail.com>
2023-05-11 00:06:29 +0900

    MorayCommentsControllerにcreateアクションを追加

--- app/controllers/api/v1/moray_comments_controller.rb
+++ app/controllers/api/v1/moray_comments_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::MorayCommentsController < ApplicationController
+  def create
+    moray = Moray.find(params[:moray_id])
+    moray_comment = current_user.moray_comments.build(moray_comment_params)
+    moray_comment.moray_id = moray.id
+    if moray_comment.save
+      render json: moray_comment, status: :created
+    else
+      render json: { errors: moray_comment.errors.messages }
+    end
+  end
+
+  def moray_comment_params
+    params.require(:moray_comment).permit(:body)
+  end
 end
