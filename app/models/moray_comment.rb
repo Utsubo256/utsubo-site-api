5628b68a6e1543adfe64ff6ea7264144d6503a2b
Utsubo256 <greeeentea256@gmail.com>
2023-05-11 10:25:19 +0900

    デフォルトでコメント一覧情報を降順で返す設定を追加

--- app/models/moray_comment.rb
+++ app/models/moray_comment.rb
@@ -3,5 +3,6 @@ class MorayComment < ApplicationRecord
 
   belongs_to :user
   belongs_to :moray
+  default_scope -> { order(created_at: :desc) }
   validates :body, presence: true, length: { maximum: 255 }
 end
