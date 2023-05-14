79d765226a8d815da1d3ef64edb158826a25024c
Utsubo256 <greeeentea256@gmail.com>
2023-05-14 11:48:40 +0900

    AquariumCommentsControllerのindexアクションを追加

--- app/controllers/api/v1/aquarium_comments_controller.rb
+++ app/controllers/api/v1/aquarium_comments_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::AquariumCommentsController < ApplicationController
+  skip_before_action :authenticate, only: %i[index]
+
+  def index
+    aquarium = Aquarium.find(params[:aquarium_id])
+    aquarium_comments = aquarium.aquarium_comments.kept
+    render json: aquarium_comments, status: :ok
+  end
+
   def create
     aquarium = Aquarium.find(params[:aquarium_id])
     aquarium_comment = current_user.aquarium_comments.build(aquarium_comment_params)
