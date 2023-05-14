d6ec33b5d8e6f3479f2c5917d59b3d87579f25ff
Utsubo256 <greeeentea256@gmail.com>
2023-05-14 11:44:29 +0900

    AquariumCommentsControllerのcreateアクションを追加

--- app/controllers/api/v1/aquarium_comments_controller.rb
+++ app/controllers/api/v1/aquarium_comments_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::AquariumCommentsController < ApplicationController
+  def create
+    aquarium = Aquarium.find(params[:aquarium_id])
+    aquarium_comment = current_user.aquarium_comments.build(aquarium_comment_params)
+    aquarium_comment.aquarium_id = aquarium.id
+    if aquarium_comment.save
+      render json: aquarium_comment, status: :created
+    else
+      render json: { errors: aquarium_comment.errors.messages }
+    end
+  end
+
+  private
+
+  def aquarium_comment_params
+    params.require(:aquarium_comment).permit(:body)
+  end
 end
