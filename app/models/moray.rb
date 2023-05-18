b9d68cbf65faa5ea3d0d753aa3e97dbc938e0c1b
Utsubo256 <greeeentea256@gmail.com>
2023-05-18 12:54:04 +0900

    ウツボ一覧をあいうえお順に並べるよう変更

--- app/models/moray.rb
+++ app/models/moray.rb
@@ -4,5 +4,5 @@ class Moray < ApplicationRecord
   has_many :exhibitions, dependent: :destroy
   has_many :aquaria, through: :exhibitions
   has_many :moray_comments, dependent: :destroy
-  default_scope -> { order(:id) }
+  default_scope -> { order(:name_ja) }
 end
