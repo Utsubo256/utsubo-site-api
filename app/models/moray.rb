26faef8c76bd4a8221ae598db04bad66ce323341
Utsubo256 <greeeentea256@gmail.com>
2023-05-18 12:50:50 +0900

    idの昇順で並ぶよう変更

--- app/models/moray.rb
+++ app/models/moray.rb
@@ -4,4 +4,5 @@ class Moray < ApplicationRecord
   has_many :exhibitions, dependent: :destroy
   has_many :aquaria, through: :exhibitions
   has_many :moray_comments, dependent: :destroy
+  default_scope -> { order(:id) }
 end
